90469090c66d319d33940fa2097903daef5a7015
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-19 00:41:20 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+def reverse_each_word(str)
+  my_array = str.split(" ")
+  new_array = []
+  my_array.collect do |words|
+    new_array << words.reverse 
+  
+end
+new_array.join(" ")
+end
+ 
+ 
+
+
+
+#iterate over the Array
+  #then reverse each word 
+  #return new array as a string 
